6dafc1cf4f5f74a5de17c8cdbf44df7e2b3886b3
jia0119 <limsj-wp24@student.tarc.edu.my>
2026-08-29 00:50:09 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "febdc1ed-a690-48b4-9f45-4f04b6d1ed95",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
